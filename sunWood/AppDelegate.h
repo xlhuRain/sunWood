2dff582d094240c1ba582532c582fa698e64dca1
xlhu_mac <tagux-mac-04@iMac-iooi.local>
2014-01-16 19:02:27 +0800

    Initial Commit

---
+++ sunWood/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sunWood
+//
+//  Created by xlhu_mac on 14-1-16.
+//  Copyright (c) 2014年 tagux-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sunWood/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sunWood
+//
+//  Created by xlhu_mac on 14-1-16.
+//  Copyright (c) 2014年 tagux-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
